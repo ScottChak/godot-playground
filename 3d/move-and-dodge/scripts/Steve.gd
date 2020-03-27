a263f53b3cb8577b2ea417daeb12f16e96c793fc
Ewan Burns <ewan@beezup.com>
2020-03-27 18:48:59 +0100

    Rolling ball

--- 3d/move-and-dodge/scripts/Steve.gd
+++ 3d/move-and-dodge/scripts/Steve.gd
@@ -1,8 +1,10 @@
 extends KinematicBody
 
-export(int) var base_speed = 8
+export(int) var max_speed = 8
 export(float) var acceleration_rate = 0.2
-export(float) var deceleration_rate = 0.05
+export(float) var deceleration_rate = 0.03
+
+export(float) var rotation_rate = 0.05
 
 var player_name = "STEEEVE"
 var velocity = Vector3(0, 0, 0)
@@ -14,19 +16,25 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
 		pass
 	elif Input.is_action_pressed("ui_right"):
-		velocity.x = lerp(velocity.x, base_speed, acceleration_rate)
+		velocity.x = lerp(velocity.x, max_speed, acceleration_rate)
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x = lerp(velocity.x, -base_speed, acceleration_rate)
+		velocity.x = lerp(velocity.x, -max_speed, acceleration_rate)
 	else:
 		velocity.x = lerp(velocity.x, 0, deceleration_rate)
 		
 	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
 		pass
 	elif Input.is_action_pressed("ui_up"):
-		velocity.z = lerp(velocity.z, -base_speed, acceleration_rate)
+		velocity.z = lerp(velocity.z, -max_speed, acceleration_rate)
 	elif Input.is_action_pressed("ui_down"):
-		velocity.z = lerp(velocity.z, base_speed, acceleration_rate)
+		velocity.z = lerp(velocity.z, max_speed, acceleration_rate)
 	else:
 		velocity.z = lerp(velocity.z, 0, deceleration_rate)
 	
 	move_and_slide(velocity)
+	$MeshInstance.rotate(Vector3.FORWARD, speedToRad(velocity.x))
+	$MeshInstance.rotate(Vector3.RIGHT, speedToRad(velocity.z))
+
+func speedToRad(speed):
+	var percent_speed = speed / max_speed
+	return PI * rotation_rate * percent_speed
